30b2589ad8708b11764e10d5e98c577e2406ba9a
David Tarkhnishvili <datotarkh@gmail.com>
2025-03-07 13:23:26 +0100

    Show a confimation dialog box

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,11 +1,36 @@
-import { Button } from "@radix-ui/themes";
+"use client";
+
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import Link from "next/link";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   return (
-    <Button color="red">
-      <Link href={`/api/issue/delete/${issueId}`}>Delete Issue</Link>
-    </Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">
+          {/* <Link href={`/api/issue/delete/${issueId}`}>Delete Issue</Link> */}
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure you want to delete this Issue? this action can not be
+          undone.
+        </AlertDialog.Description>
+        <Flex gap="3" mt="4" justify="end">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button variant="solid" color="red">
+              <Link href={`/api/issue/delete/${issueId}`}>Delete Issue</Link>
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   );
 };
 
