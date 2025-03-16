56c0e34942745969594a201637d3b2afb4e6e50b
David Tarkhnishvili <datotarkh@gmail.com>
2025-03-16 11:52:33 +0100

    Add comment section

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,5 +1,5 @@
 import prisma from "@/app/lib/prisma";
-import { Box, Flex, Grid } from "@radix-ui/themes";
+import { Avatar, Box, Card, Flex, Grid, Separator, Text } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 import EditIssueButton from "./EditIssueButton";
 import IssueDetails from "./IssueDetails";
@@ -7,6 +7,7 @@ import DeleteIssueButton from "./DeleteIssueButton";
 import { auth } from "@/auth";
 import SelectUser from "./SelectUser";
 import { cache } from "react";
+import ReactMarkdown from "react-markdown";
 
 interface Props {
   params: { id: string };
@@ -25,6 +26,7 @@ const IssueDetailPage = async ({ params }: Props) => {
     <Grid columns={{ initial: "1", sm: "5" }} gap="5">
       <Box className="md:col-span-4">
         <IssueDetails issue={issue} />
+        <CommentForm issueId={params.id}/>
       </Box>
       <Box className="col-span-1">
         {session && (
@@ -39,9 +41,49 @@ const IssueDetailPage = async ({ params }: Props) => {
   );
 };
 
+const CommentForm = async ({ issueId}: {issueId: string}) => {
+  const comments = await prisma.comment.findMany({
+    where: { issueId: parseInt(issueId)  },
+    include: {
+      user: true,
+    },
+  });
 
+  if(!comments) return null;
 
+  return (
+    <>
+      <Text className="text-lg font-bold">Comments</Text>
+      <Separator my="3" />
 
+      {/* {comments.length === 0 ? (
+        <Text>No comments yet. Be the first to comment!</Text>
+      ) : ( */}
+        {(comments.map((comment) => (
+          <Card key={comment.id}>
+          <Flex  direction="column" gap="2" className="mb-4">
+            <Flex align="center" gap="3">
+              <Avatar
+                src={comment.user?.image || "https://via.placeholder.com/40"}
+                fallback="?"
+                size="3"
+              />
+              <Flex direction="column">
+                <Text className="font-medium">{comment.user?.name || "Anonymous"}</Text>
+                <Text className="text-sm text-gray-500">
+                  {new Date(comment.createdAt).toLocaleDateString()}
+                </Text>
+              </Flex>
+            </Flex>
+            <ReactMarkdown>{comment.content}</ReactMarkdown>
+            <Separator my="2" />
+          </Flex>
+           </Card>
+        ))
+      )}
+       </>
+  );
+};
 
 export async function generateMetadata({ params }: Props) {
   const issue = await fetchUser(parseInt(params.id));
